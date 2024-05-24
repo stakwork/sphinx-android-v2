11121e12458e282720bfd91fb84c22f5c4af4c60
Alejandro <ariel10aguero@gmail.com>
2024-05-24 11:25:28 -0300

    Test generate xpub key

--- sphinx/application/data/features/feature-connect-manager/src/androidTest/java/chat/sphinx/feature_connect_manager/ExampleInstrumentedTest.kt
+++ sphinx/application/data/features/feature-connect-manager/src/androidTest/java/chat/sphinx/feature_connect_manager/ExampleInstrumentedTest.kt
@@ -7,6 +7,9 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.junit.Before
 import uniffi.sphinxrs.mnemonicFromEntropy
+import uniffi.sphinxrs.mnemonicToSeed
+import uniffi.sphinxrs.rootSignMs
+import uniffi.sphinxrs.xpubFromSeed
 import java.security.SecureRandom
 
 /**
@@ -17,24 +20,53 @@ import java.security.SecureRandom
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     private lateinit var connectManager: ConnectManagerImpl
+    private var seed: String? = null
+    private val lspIp = "34.229.52.200:1883"
+    private var xpub: String? = null
+    private var sign: String? = null
+    private val network = "regtest"
+
     @Before
     fun setUp() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         connectManager = ConnectManagerImpl()
+        generateMnemonicAndSeed()
     }
 
     @OptIn(ExperimentalUnsignedTypes::class)
-    @Test
-    fun generateMnemonic() {
-        var mnemonic: String? = null
-        try {
-            val randomBytes = generateRandomBytes(16)
-            val randomBytesString = randomBytes.joinToString("") { it.toString(16).padStart(2, '0') }
-            mnemonic = mnemonicFromEntropy(randomBytesString)
-        } catch (e: Exception) {
-            println("Error generating mnemonic: ${e.message}")
-        }
+    private fun generateMnemonicAndSeed() {
+        val randomBytes = generateRandomBytes(16)
+        val randomBytesString = randomBytes.joinToString("") { it.toString(16).padStart(2, '0') }
+        val mnemonic = mnemonicFromEntropy(randomBytesString)
         assertNotNull(mnemonic)
+
+        val generatedSeed = mnemonic.let { mnemonicToSeed(it) }
+        seed = generatedSeed
+        assertNotNull(seed)
+    }
+
+    @Test
+    fun generateXpub() {
+        assertNotNull("Seed should be generated", seed)
+        val myXpub = xpubFromSeed(seed!!, getTimestampInMilliseconds(), network)
+        xpub = myXpub
+        assertNotNull(xpub)
+    }
+
+    @Test
+    fun signMsg() {
+        assertNotNull("Seed should be generated", seed)
+        val generatedSign = rootSignMs(seed!!, getTimestampInMilliseconds(), network)
+        sign = generatedSign
+        assertNotNull(sign)
+    }
+
+    @Test
+    fun useSignForAnotherMethod() {
+        assertNotNull("Sign should be generated", sign)
+
+        val anotherResult = anotherMethodThatUsesSign(sign!!)
+        assertNotNull(anotherResult)
     }
 
     private fun generateRandomBytes(size: Int): UByteArray {
@@ -46,8 +78,14 @@ class ExampleInstrumentedTest {
         for (i in bytes.indices) {
             uByteArray[i] = bytes[i].toUByte()
         }
-
         return uByteArray
     }
 
+    private fun getTimestampInMilliseconds(): String =
+        System.currentTimeMillis().toString()
+
+    private fun anotherMethodThatUsesSign(sign: String): String {
+        // Your logic here
+        return "Result based on sign: $sign"
+    }
 }

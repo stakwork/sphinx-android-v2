78ff8f24f5e1e7eba90b27408ac97f18a1d14e64
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-10-29 16:33:45 -0300

    Fixes on auth process

--- sphinx/application/network/concepts/queries/concept-network-query-authorize-external/src/main/java/chat/sphinx/concept_network_query_verify_external/model/PersonInfoDto.kt
+++ sphinx/application/network/concepts/queries/concept-network-query-authorize-external/src/main/java/chat/sphinx/concept_network_query_verify_external/model/PersonInfoDto.kt
@@ -4,12 +4,11 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class PersonInfoDto(
-    val id: Int?,
+    val id: Int? = null,
     val owner_pubkey: String,
     val owner_alias: String,
-    val owner_contact_key: String?,
     val owner_route_hint: String?,
     val img: String?,
-    val description: String?,
-    val price_to_meet: Long?
+    val description: String? = null,
+    val price_to_meet: Long? = null
 )

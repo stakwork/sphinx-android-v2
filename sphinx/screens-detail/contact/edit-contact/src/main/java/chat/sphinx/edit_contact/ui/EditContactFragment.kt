eb6911ce7bd43fb9ca6329d645df8f62928df23d
AnonKotlinDev <tasksfreelance7@gmail.com>
2025-02-28 22:04:04 -0300

    Implemented the "Share timezone" UI feature in the EditContactFragment.kt

--- sphinx/screens-detail/contact/edit-contact/src/main/java/chat/sphinx/edit_contact/ui/EditContactFragment.kt
+++ sphinx/screens-detail/contact/edit-contact/src/main/java/chat/sphinx/edit_contact/ui/EditContactFragment.kt
@@ -1,7 +1,17 @@
 package chat.sphinx.edit_contact.ui
 
+import android.icu.util.TimeZone
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -12,6 +22,7 @@ import chat.sphinx.contact.databinding.LayoutContactSaveBinding
 import chat.sphinx.contact.ui.ContactFragment
 import chat.sphinx.edit_contact.R
 import chat.sphinx.edit_contact.databinding.FragmentEditContactBinding
+import chat.sphinx.resources.setTextColorExt
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,8 +32,8 @@ internal class EditContactFragment : ContactFragment<
         FragmentEditContactBinding,
         EditContactFragmentArgs,
         EditContactViewModel
-        >(R.layout.fragment_edit_contact)
-{
+        >(R.layout.fragment_edit_contact) {
+
     @Inject
     lateinit var userColorsHelperInj: UserColorsHelper
 
@@ -30,6 +41,7 @@ internal class EditContactFragment : ContactFragment<
         get() = userColorsHelperInj
 
     override val viewModel: EditContactViewModel by viewModels()
+
     override val binding: FragmentEditContactBinding by viewBinding(FragmentEditContactBinding::bind)
 
     override val headerBinding: LayoutContactDetailScreenHeaderBinding by viewBinding(
@@ -43,12 +55,38 @@ internal class EditContactFragment : ContactFragment<
         LayoutContactSaveBinding::bind, R.id.include_edit_contact_layout_save
     )
 
+    private lateinit var fragmentEditContactBinding: FragmentEditContactBinding
+
     override fun getHeaderText(): String = getString(R.string.edit_contact_header_name)
 
     override fun getSaveButtonText(): String = getString(R.string.save_contact_button)
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentEditContactBinding = FragmentEditContactBinding.inflate(inflater)
+        return fragmentEditContactBinding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val allTimezonesList: List<String> =
+            TimeZone.getAvailableIDs()
+                .toMutableList()
+                .also { it.add(index = 0, element = "Use Computer Settings") }
+                .toList()
+
+        val spinnerAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            allTimezonesList
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
 
         headerBinding.apply {
             textViewDetailScreenSubscribe.setOnClickListener {
@@ -57,5 +95,63 @@ internal class EditContactFragment : ContactFragment<
                 }
             }
         }
+
+        fragmentEditContactBinding.apply {
+
+            spinnerTimezones.let {
+                it.adapter = spinnerAdapter
+                it.setSelection(0)
+
+                disableSpinner(
+                    spinner = it,
+                    spinnerLabel = textViewContactTimezone
+                )
+
+                it.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        parent?.setSelection(position)
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {}
+                }
+            }
+
+            switchEditContact.setOnCheckedChangeListener { compoundButton, isChecked ->
+                spinnerTimezones.let { it ->
+                    if (!isChecked) {
+                        disableSpinner(spinner = it, spinnerLabel = textViewContactTimezone)
+                    } else {
+                        enableSpinner(spinner = it, spinnerLabel = textViewContactTimezone)
+                    }
+                }
+            }
+        }
     }
+
+    private fun disableSpinner(spinner: Spinner, spinnerLabel: AppCompatTextView) {
+        spinner.apply {
+            isEnabled = false
+            isClickable = false
+            alpha = 0.5f
+            setSelection(0)
+        }
+
+        spinnerLabel.setTextColorExt(chat.sphinx.resources.R.color.secondaryText)
+    }
+
+    private fun enableSpinner(spinner: Spinner, spinnerLabel: AppCompatTextView) {
+        spinner.apply {
+            isEnabled = true
+            isClickable = true
+            alpha = 1.0f
+        }
+
+        spinnerLabel.setTextColorExt(chat.sphinx.resources.R.color.text)
+    }
+
 }

1e82da055725a48d546385741bb53e7c3633f4a6
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-07-13 11:13:21 -0300

    Start appium script fixed

--- scripts/start-appium.sh
+++ scripts/start-appium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bash /root/sphinx-android-v2/scripts/wait-for-emulator.sh
-/home/codespace/nvm/current/bin/appium --address 0.0.0.0 --port 4723 --default-capabilities '{\"appium:automationName\":\"UiAutomator2\"}'
+/home/codespace/nvm/current/bin/appium --address 0.0.0.0 --port 4723 --default-capabilities '{"appium:automationName":"UiAutomator2"}'

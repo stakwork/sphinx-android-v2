d301991ad229e99f3505d722c66110f25870ede5
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-07-17 09:47:18 -0300

    Start android script updated to install APK and launch app during build

--- scripts/start-android.sh
+++ scripts/start-android.sh
@@ -3,7 +3,7 @@ bash /root/sphinx-android-v2/scripts/wait-for-emulator.sh
 ./gradlew assembleDebug --no-daemon
 
 # Install the APK on the emulator
-adb install -r sphinx/application/sphinx/build/outputs/apk/debug/sphinx-universal-debug.apk
+adb install -r sphinx/application/sphinx/build/outputs/apk/debug/sphinx-x86_64-debug.apk
 
 # Launch the app
-adb shell am start -n chat.sphinx.v2.debug/chat.sphinx.MainActivity
+adb shell am start -n chat.sphinx.v2.debug/chat.sphinx.activitymain.MainActivity

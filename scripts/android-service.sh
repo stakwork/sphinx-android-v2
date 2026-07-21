94842aacf39381a58c15193a57a2feaf8adddfa7
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-07-21 10:52:26 -0300

    New script added Android script updated

--- scripts/android-service.sh
+++ scripts/android-service.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if adb shell pidof chat.sphinx.v2.debug > /dev/null 2>&1; then
+  echo "App already running, skipping install and launch"
+  sleep infinity
+fi
+
 bash /workspaces/sphinx-android-v2/scripts/wait-for-emulator.sh
 
 ./gradlew assembleDebug --no-daemon
@@ -8,3 +14,5 @@ adb install -r sphinx/application/sphinx/build/outputs/apk/debug/sphinx-x86_64-d
 
 # Launch the app
 adb shell am start -n chat.sphinx.v2.debug/chat.sphinx.activitymain.MainActivity
+
+sleep infinity

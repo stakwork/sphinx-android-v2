38e865c535253d0efcb1c4f0ba45205490a08ed2
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-07-21 11:05:22 -0300

    Scripts updated

--- scripts/android-service.sh
+++ scripts/android-service.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
+bash /workspaces/sphinx-android-v2/scripts/wait-for-emulator.sh
+
 if adb shell pidof chat.sphinx.v2.debug > /dev/null 2>&1; then
   echo "App already running, skipping install and launch"
   sleep infinity
 fi
 
-bash /workspaces/sphinx-android-v2/scripts/wait-for-emulator.sh
-
-./gradlew assembleDebug --no-daemon
-
-# Install the APK on the emulator
-adb install -r sphinx/application/sphinx/build/outputs/apk/debug/sphinx-x86_64-debug.apk
-
-# Launch the app
-adb shell am start -n chat.sphinx.v2.debug/chat.sphinx.activitymain.MainActivity
+bash /workspaces/sphinx-android-v2/scripts/start-android.sh
 
 sleep infinity
--- scripts/start-android.sh
+++ scripts/start-android.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 bash /workspaces/sphinx-android-v2/scripts/wait-for-emulator.sh
 
 ./gradlew assembleDebug --no-daemon
+if [ $? -ne 0 ]; then
+  echo "BUILD FAILED - see error above"
+  exit 1
+fi
 
-# Install the APK on the emulator
 adb install -r sphinx/application/sphinx/build/outputs/apk/debug/sphinx-x86_64-debug.apk
-
-# Launch the app
 adb shell am start -n chat.sphinx.v2.debug/chat.sphinx.activitymain.MainActivity
+
+echo "App successfully built, installed and launched"

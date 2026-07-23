67fff02227392f61ce964a49e7884ee2eb6d1a14
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-07-23 16:40:23 -0300

    Sleep infinity added back in android-service

--- scripts/android-service.sh
+++ scripts/android-service.sh
@@ -5,3 +5,5 @@ echo "=== android-service.sh called at $(date) ===" >> /tmp/android-build.log
 bash /workspaces/sphinx-android-v2/scripts/wait-for-emulator.sh
 
 bash /workspaces/sphinx-android-v2/scripts/start-android.sh
+
+sleep infinity

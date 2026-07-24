f9dbf9c1dbd1f3eef6bc13a474d37d05c1b84a1e
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-07-23 21:33:08 -0300

    android-service script updated

--- scripts/android-service.sh
+++ scripts/android-service.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-echo "=== android-service.sh called at $(date) ===" >> /tmp/android-build.log
-
-bash /workspaces/sphinx-android-v2/scripts/wait-for-emulator.sh
-
-bash /workspaces/sphinx-android-v2/scripts/start-android.sh
-
 sleep infinity

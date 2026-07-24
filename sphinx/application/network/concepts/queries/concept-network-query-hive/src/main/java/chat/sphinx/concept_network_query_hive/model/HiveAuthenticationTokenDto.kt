ad64245f6be43d921c1050ab53a775cb9e8aa7e1
tomastiminskas <tomastiminskas@users.noreply.github.com>
2026-07-24 13:32:53 +0000

    Generated with Hive: Create concept-network-query-hive and feature-network-query-hive modules for Hive authentication

---
+++ sphinx/application/network/concepts/queries/concept-network-query-hive/src/main/java/chat/sphinx/concept_network_query_hive/model/HiveAuthenticationTokenDto.kt
@@ -0,0 +1,10 @@
+package chat.sphinx.concept_network_query_hive.model
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class HiveAuthenticationTokenDto(val token: String) {
+    init {
+        require(token.isNotBlank()) { "Hive token must not be blank" }
+    }
+}
